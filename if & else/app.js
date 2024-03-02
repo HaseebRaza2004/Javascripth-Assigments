34d56ded4b54eecbafcdb088c522210754ce19fb
haseeb raza <hseebraza2004@gmail.com>
2024-03-02 23:25:42 +0500

    ok

---
+++ if & else/app.js
@@ -0,0 +1,11 @@
+// // Q:1
+var number = prompt('enter number')
+
+if (number % 2 == 0) {console.log(`${number} is even`)}
+else{console.log(`${number} is odd`)}
+
+// Q:2
+var num = prompt('enter number')
+
+if(number % 2 == 0){console.log(`the number is positive`)}
+else{console.log(`the number is negative`)}
