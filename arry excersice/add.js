e94ec87a218b301a2fc4e8ac59ca03e43c8b916d
haseeb raza <hseebraza2004@gmail.com>
2024-03-18 17:23:38 +0500

    arry excersice 10 questions completed

---
+++ arry excersice/add.js
@@ -0,0 +1,75 @@
+// Q:1
+var students_name = []
+
+students_name.push("ali")
+students_name.push("ahmed")
+students_name.push("bilal")
+console.log(students_name)
+
+// Q:2
+var future_students = []
+
+future_students["studet_1"] = "umer"
+future_students["studet_2"] = "usamn"
+future_students["studet_3"] = "siddiq"
+
+console.log(future_students)
+
+// Q:3
+var sting_arry = ["apple", "banana", "mango", "orange"]
+console.log(sting_arry)
+
+// Q:4
+var number_arry = [1, 2, 3, 4]
+console.log(number_arry)
+
+// Q:5
+var boolean_arry = [true, false]
+console.log(boolean_arry)
+
+// Q:6
+var mixed_arry = ["sting", 123, true, { name: "Haseeb", age: 19 }]
+console.log(mixed_arry)
+
+// Q:7
+var mobile_netwoks_in_PAKISTAN = ["Jazz", "Warid", "Telenor", "Ufone", "Zong", "Onic"]
+console.log(mobile_netwoks_in_PAKISTAN)
+
+// Question 8
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.PHIL", "PHD"]
+document.write(" <br> <h2> Qualification in Pakistan </h2> <br> ")
+document.write(" <br> 1:", qualifications[0])
+document.write(" <br> 2:", qualifications[1])
+document.write(" <br> 3:", qualifications[2])
+document.write(" <br> 4:", qualifications[3])
+document.write(" <br> 5:", qualifications[4])
+document.write(" <br> 6:", qualifications[5])
+document.write(" <br> 7:", qualifications[6])
+document.write(" <br> 8:", qualifications[7])
+
+// Question 9
+var movies = [];
+movies.push("Avengers")
+movies.push("Antman")
+movies.push("Superman")
+movies.push("Deadpool")
+movies.push("Guardian of the galaxy")
+
+var mlength = (movies.length)
+document.write("<h2> <br> TOP MOVIES OF 2015 </h2>")
+document.write(" <br> ", movies[0])
+document.write(" <br> ", movies[1])
+document.write(" <br> ", movies[2])
+document.write(" <br> ", movies[3])
+document.write(" <br> ", movies[4])
+
+document.write("<h2> length of an array: </h2>", mlength)
+
+// Q:10
+var cars = ["audi","volvo","toyota","lamborghini","ferarri","ford"]
+
+document.write("<h2> super cars <h2/>")
+document.write("<br> first index of the arry:",cars[0] )
+document.write("<br> car at first index of the arry:",cars[0] )
+document.write("<br> last index of the arry:",cars[5] )
+document.write("<br> car at last index of the arry:",cars[5] )
