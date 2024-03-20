05b5c3665192fdac838550f79cfb2dc4928ab3ff
haseeb raza <hseebraza2004@gmail.com>
2024-03-20 17:07:40 +0500

    loop ecsercise added

---
+++ loop excersice/app.js
@@ -0,0 +1,37 @@
+
+// Q:1
+for (var a = 1 ; a <= 10 ; a++){console.log(a)} 
+
+// Q:2
+for (var a = 1 ; a <= 20 ; a++){
+    if(a % 2 == 0 ){console.log(a)}
+}
+
+// Q:3
+for(var a = 1 ; a <= 15 ; a++){
+    if(a % 2 == 1){console.log(a)}
+}
+
+// Q:4
+
+
+
+
+// Q:5
+for(var a = 1 ; a <= 10 ; a++){
+    console.log(7 * a)
+}
+
+// Q:6
+let b = 0;
+let c = 1;
+
+for (let i = 3; i <= 10; i++) {
+    let d = b + c;
+    console.log(d); 
+    b = c;
+    c = d;
+}
+
+// Q:7
+
