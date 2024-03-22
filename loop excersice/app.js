622198b36801ef4b3de444c8e98ff4f9ca8e79c8
haseeb raza <hseebraza2004@gmail.com>
2024-03-22 16:53:46 +0500

    ok

--- loop excersice/app.js
+++ loop excersice/app.js
@@ -1,15 +1,15 @@
 
 // Q:1
-for (var a = 1 ; a <= 10 ; a++){console.log(a)} 
+for (var a = 1; a <= 10; a++) { console.log(a) }
 
 // Q:2
-for (var a = 1 ; a <= 20 ; a++){
-    if(a % 2 == 0 ){console.log(a)}
+for (var a = 1; a <= 20; a++) {
+    if (a % 2 == 0) { console.log(a) }
 }
 
 // Q:3
-for(var a = 1 ; a <= 15 ; a++){
-    if(a % 2 == 1){console.log(a)}
+for (var a = 1; a <= 15; a++) {
+    if (a % 2 == 1) { console.log(a) }
 }
 
 // Q:4
@@ -18,7 +18,7 @@ for(var a = 1 ; a <= 15 ; a++){
 
 
 // Q:5
-for(var a = 1 ; a <= 10 ; a++){
+for (var a = 1; a <= 10; a++) {
     console.log(7 * a)
 }
 
@@ -28,7 +28,7 @@ let c = 1;
 
 for (let i = 3; i <= 10; i++) {
     let d = b + c;
-    console.log(d); 
+    console.log(d);
     b = c;
     c = d;
 }
@@ -38,7 +38,14 @@ for (let i = 3; i <= 10; i++) {
 // Q:8
 
 // Q:9
-for(var i = 1 ; i <= 10 ; i++){console.log(i*i)}
+for (var i = 1; i <= 10; i++) { console.log(i * i) }
 
 // Q:10
+var num = [3, 7, 2, 9, 5]
+var largestnum = 0
+
+for(var i = 0; i < num.length; i++){
+    if(num[i] > largestnum){largestnum = num[i]}
+}
+console.log(largestnum)
 
