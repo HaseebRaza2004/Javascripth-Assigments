41e76bbb42573f3b7f08e2ddb598968eb4fd5a88
haseeb raza <hseebraza2004@gmail.com>
2024-05-02 14:31:46 +0500

    clock completed

--- Clock/app.js
+++ Clock/app.js
@@ -2,15 +2,22 @@
 var hours = document.getElementById("hours");
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
+var am_pm = document.getElementById("am/pm")
 
 setInterval(function(){
     var curentDate = new Date();
     var curentHour = curentDate.getHours();
 // console.log(curentDate);
 // console.log(curentHour);
-    hours.innerText = curentDate.getHours();
-    minutes.innerText = curentDate.getMinutes();
-    seconds.innerText = curentDate.getSeconds();
+    hours.innerText = curentDate.getHours().toString().padStart(2, "0");
+    minutes.innerText = curentDate.getMinutes().toString().padStart(2, "0");
+    seconds.innerText = curentDate.getSeconds().toString().padStart(2, "0");
+
+    if (hours.innerText < 12) {
+        am_pm.innerText = "AM"
+    } else {
+        am_pm.innerText = "PM"
+    }
 }, 1000);
 
 var date = document.getElementById("date")
